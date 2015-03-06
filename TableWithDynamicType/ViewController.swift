9731e458c5d85e7d3664c424e2c5286ed4ff74f5
samuraipapa <yury.gitman@gmail.com>
2015-03-06 13:05:24 -0500

    adapted from MasterDetailView Default App.  It's crashes when runs

--- TableWithDynamicType/ViewController.swift
+++ TableWithDynamicType/ViewController.swift
@@ -6,9 +6,13 @@
 //  Copyright (c) 2015 yuryg. All rights reserved.
 //
 
+// Show Self-Sizing Cell and Dynamic Type
+
 import UIKit
 
-class ViewController: UIViewController {
+
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel!.text = "hello"
+        
+        return cell
 
+    }
+    
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+       return 5
+        
+        //<#code#>
+    }
+    
 }
 
